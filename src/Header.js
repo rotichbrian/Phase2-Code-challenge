ce74226af9e97c08f8557185d202a6eb50b74f84
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-28 18:16:50 +0300

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    // <div>Header</div>
+    <>
+    <h1 className='header'>The Royal Bank Of Flatiron</h1>
+    </>
+  )
+}
+
+export default Header
